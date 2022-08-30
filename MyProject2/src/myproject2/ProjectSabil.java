0aa0679aa4eebfc4156600ca1277675ebe216520
salsabilarp <89761663+salsabilarp@users.noreply.github.com>
2022-08-30 09:39:52 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/ProjectSabil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class ProjectSabil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("SSSSSSS  ");
+        System.out.print("    AAAAA     ");
+        System.out.println(" LL");
+        System.out.print("SS       ");
+        System.out.print("   AA   AA  ");
+        System.out.println("   LL");
+        System.out.print("SSSSSSS  ");
+        System.out.print("  AA AAA AA  ");
+        System.out.println("  LL   ");
+        System.out.print("     SS  ");
+        System.out.print(" AA       AA   ");
+        System.out.println("LL   ");
+        System.out.print("SSSSSSS  ");
+        System.out.print("AA         AA  ");
+        System.out.println("LLLLLLL   ");
+    }
+    
+}
